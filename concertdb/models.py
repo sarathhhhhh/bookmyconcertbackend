d9f045e5aad4013bcaf80fc5e60aba46a3c58649
sarathhhhhh <sarathsastham@gmail.com>
2026-02-20 11:35:34 +0530

    last

--- concertdb/models.py
+++ concertdb/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+class Concert(models.Model):
+    name = models.CharField(max_length=30)
+    date_time=models.DateTimeField()
+    venue=models.CharField(max_length=100)
+    ticket_price=models.DecimalField(max_digits=8,decimal_places=2)
+    available_tickets=models.PositiveIntegerField()
+
+class Booking(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    concert=models.ForeignKey(Concert,on_delete=models.CASCADE)
+    tickets_booked=models.PositiveIntegerField()
+    
+    class Meta:
+        unique_together = ('user', 'concert')
+
+
+
+
+    
+
